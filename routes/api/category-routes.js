d5dfdd959f2ae7f77e3f74cfc1a23036fcaff980
Elisa Vidal <eunice1vidal@yahoo.com>
2022-03-16 09:16:29 -0700

    routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,29 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include:[
+      {
+       model: Product,
+       attributes:[ 'product_name', 'pric', 'stock']
+      }
+    ]
+  }).then(user => {
+    console.log(user);
+    res.json(user)
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    
+  })
 });
 
 router.post('/', (req, res) => {
