0c2dab49c88ee1c00a2eeb810a6b8aa5e4964266
Elisa Vidal <eunice1vidal@yahoo.com>
2022-03-23 23:37:56 -0700

    routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { json } = require('express/lib/response');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
--- routes/api/tags-routes.js
+++ routes/api/tags-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { json } = require('express/lib/response');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
